b39b50012ca5f97694238d67330535ef047ba4ac
alyelaswad <124714695+alyelaswad@users.noreply.github.com>
2023-04-16 16:29:53 +0200

    Add files via upload

--- deque.h
+++ deque.h
@@ -1,9 +1,26 @@
+#ifndef deque_H
+#define deque_H
 #include <iostream>
 using namespace std;
-class deque
+#include "Node.h"
+// #include "deque.cpp"
+template <typename T>
+class Deque
 {
 private:
+    Node<T> *front, *rear;
+    int dequeLength;
+
 public:
-    deque();
-    ~deque();
+    Deque<T>();
+    ~Deque<T>();
+    int DEQ_Length();
+    bool DEQisEmpty();
+    void Add_Front();
+    void Add_Rear();
+    T Remove_Front();
+    T Remove_Rear();
+    Node<T> *View_Front();
+    Node<T> *View_Rear();
 };
+#endif
