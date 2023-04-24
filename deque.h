df09cf80396f7efa3af5c7b313f3dd09e9b5473f
alyelaswad <124714695+alyelaswad@users.noreply.github.com>
2023-04-24 20:54:42 +0200

    Update deque.h

--- deque.h
+++ deque.h
@@ -16,13 +16,13 @@ private:
 public:
     Deque<T>();
     ~Deque<T>();
-    int DEQ_Length();
-    bool DEQisEmpty();
-    void Add_Front();
-    void Add_Rear();
-    T Remove_Front();
-    T Remove_Rear();
-    Node<T> *View_Front();
-    Node<T> *View_Rear();
+    int DEQ_Length(); // returns the length of the deque.
+    bool DEQisEmpty(); // checks if the deque is empty.
+    void Add_Front(); // adds at the start of the deque
+    void Add_Rear(); // adds at the end of the deque.
+    T Remove_Front(); // removes from the start of the deque.
+    T Remove_Rear(); // removes from the back/rear of the deque.
+    Node<T> *View_Front(); // returns the frontal node of the deque.
+    Node<T> *View_Rear(); // returns the back node of the deque.
 };
 #endif
