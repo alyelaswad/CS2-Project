cbd1e622c07a776bc566ab2765645f247cbd69ca
alyelaswad <124714695+alyelaswad@users.noreply.github.com>
2023-04-24 20:49:56 +0200

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,12 +1,12 @@
-#ifndef Node_H
+#ifndef Node_H // to avoid redifinition when including in main
 #define Node_H
 template <class T>
 struct Node
 {
-    Node<T> *next;
+    Node<T> *next; // since the deque can be modified from both sides, a doubly node is needed.
     Node<T> *back;
 
-    T info;
+    T info; // the data that the node will carry (ex: int, plane information, etc..)
     Node()
     {
         next = nullptr;
