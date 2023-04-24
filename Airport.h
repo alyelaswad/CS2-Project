a0fef76118a3819cb38ebea02068380d9e76afdc
alyelaswad <124714695+alyelaswad@users.noreply.github.com>
2023-04-24 21:12:40 +0200

    Update Airport.h

--- Airport.h
+++ Airport.h
@@ -2,19 +2,19 @@
 #include <vector>
 #include "AirplaneWaitingQueue.h"
 using namespace std;
-
+// this is a class that we came up with to have more of our ideas and implement extra functionalities like sponsors and gate numbers.
 class Airport
 {
 
 private:
-    static int arrivals;
-    int nSponsor;
-    vector<string> sponsors;
-    int gateNumber;
-    AirplaneWaitingQueue<int> WaitingQueue;
+    static int arrivals; // number of arrivals from that landing way.
+    int nSponsor; // number of sponsors
+    vector<string> sponsors; // vector of the sponsors.
+    int gateNumber; // gate number to expect the arrivals.
+    AirplaneWaitingQueue<int> WaitingQueue; // composition of the queue to be implemented.
 
 public:
-    vector<string> getSponsors();
+    vector<string> getSponsors(); // getters and setters.
 
     void setSponsors(vector<string> sponsors);
 
@@ -24,12 +24,12 @@ public:
 
     static int getArrivals();
 
-    void displayLogs();
+    void displayLogs(); // displays the logs of simulating the queue.
 
-    bool gateisavailable();
+    bool gateisavailable(int n); // checks if a certain gate is available
 
-    Airport();
+    Airport(); //constructor and destructor
     ~Airport();
 
-    void ComputeQueue();
+    void ComputeQueue(); // operates the queue with all its functionalities.
 };
