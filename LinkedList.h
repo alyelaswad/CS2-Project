13976026aadcd650e5938a8f851c7d7b68f3c63f
alyelaswad <124714695+alyelaswad@users.noreply.github.com>
2023-04-24 20:52:33 +0200

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,22 +1,22 @@
 #ifndef LinkedList_H
 #define LinkedList_H
-#include "Node.h"
+#include "Node.h" // including the node, since it is associated with the linked list.
 template <class T>
 class LinkedList
 {
 private:
-    Node<T> *head, *tail;
+    Node<T> *head, *tail; // doubly linked list's nodes.
 
 public:
     LinkedList();
 
-    Node<T> *get_head();
+    Node<T> *get_head(); // getter for head
 
-    Node<T> *get_tail();
+    Node<T> *get_tail(); // getter for tail
 
-    Node<T> *append(T info_p);
+    Node<T> *append(T info_p); // inserts at the end of the node
 
-    Node<T> *insertMid(Node<T> *curr, T info);
-    void display(LinkedList<T> node);
+    Node<T> *insertMid(Node<T> *curr, T info); // inserts between between nodes (could also be used to insert at the start of the linked list.)
+    void display(LinkedList<T> node); //(displays the list's contents)
 };
 #endif
